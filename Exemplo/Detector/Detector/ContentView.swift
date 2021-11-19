2d8fc7fb9bc5bc5a0d0553d33b22b8d3269aae28
Pedro Henrique Ferreira Figueiredo <pedro.figueiredo@castgroup.com.br>
2021-11-18 22:33:04 -0300

    Exemplo Aula 02

---
+++ Exemplo/Detector/Detector/ContentView.swift
@@ -0,0 +1,75 @@
+//
+//  ContentView.swift
+//  Detector
+//
+//  Created by Pedro Henrique on 18/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject
+    var pickerViewModel = ImageAcquisitionController()
+    
+    @ObservedObject
+    var detectorViewModel = Detector()
+
+    @ViewBuilder
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Coloque a foto de uma flor para detectar a espécie")
+                
+                
+                if detectorViewModel.loading {
+                    VStack(alignment: .center) {
+                        Spacer()
+                        ProgressView()
+                        Text("Aguarde... identificando...")
+                        Spacer()
+                    }
+                }else {
+                    if let selectedImage = pickerViewModel.selectedImage {
+                        Image(uiImage: selectedImage)
+                            .resizable()
+                            .scaledToFit()
+                    }
+                    
+                    if let result = detectorViewModel.detectorResult {
+                        Text(result)
+                    }
+                    
+                }
+                
+                HStack {
+                    
+                    Button("Usar a câmera") {
+                        pickerViewModel.takePhoto()
+                    }
+                    Button("Escolher foto") {
+                        pickerViewModel.choosePhoto()
+                    }
+                    
+                }
+            }
+            .padding()
+            .navigationTitle("Detector")
+            .fullScreenCover(isPresented: $pickerViewModel.isPresentingPicker) {
+                ImagePicker(sourceType: pickerViewModel.sourceType) { image in
+                    if let image = image {
+                        detectorViewModel.onImageSelected(image)
+                        pickerViewModel.selectedImage = image
+                    }
+                    pickerViewModel.isPresentingPicker = false
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Exemplo/Detector/Detector/ImageAcquisitionController.swift
@@ -0,0 +1,33 @@
+//
+//  CameraController.swift
+//  Detector
+//
+//  Created by Pedro Henrique on 18/11/21.
+//
+
+import Foundation
+import UIKit.UIImage
+
+class ImageAcquisitionController: ObservableObject {
+    
+    private(set) var sourceType = ImagePicker.SourceType.photoLibrary
+    
+    @Published
+    var selectedImage: UIImage?
+    
+    @Published
+    var isPresentingPicker = false
+    
+    
+    func takePhoto() {
+        sourceType = .camera
+        isPresentingPicker = true
+    }
+    
+    func choosePhoto() {
+        sourceType = .photoLibrary
+        isPresentingPicker = true
+        
+    }
+    
+}
